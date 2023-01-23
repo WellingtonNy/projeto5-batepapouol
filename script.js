e7314a0cfb5225bb9c4ecd928badaa7e931fa88d
Wellington <Wellingtoncpfilho@gmail.com>
2023-01-23 13:51:32 -0300

    detalhes tecnicos

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const tela = document.querySelector('.lista');
 
 let lastMsg;
 
+const intervalo5s = 5000
+const intervalo3s = 3000
+
 const resposta = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', nome);
 
 resposta.then(iniciar);
@@ -14,13 +17,13 @@ resposta.catch(recarregar);
 
 function iniciar() {
     carregarMensagens();
-    setInterval(carregarMensagens, 3000);
-    setInterval(statusAtivo, 5000);
+    setInterval(carregarMensagens, intervalo3s);
+    setInterval(statusAtivo, intervalo5s);
 }
 
 
 function carregarMensagens() {
-    const dados = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages')
+    const dados = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
     dados.then(exibirNaTela);
 }
 
@@ -31,15 +34,28 @@ function exibirNaTela(dados) {
     for (let i = 0; i < mensagens.length; i++) {
         if (mensagens[i].type === 'status') {
 
-            tela.innerHTML += `<li data-test="message" class="mensagem status"> <span>(${mensagens[i].time})</span><b>${mensagens[i].from}</b>  ${mensagens[i].text}</li>`;
+            tela.innerHTML +=
+                `<li data-test="message" class="mensagem status">
+             <span>(${mensagens[i].time})</span> <b>${mensagens[i].from}
+             </b>:  ${mensagens[i].text}</li>`;
 
         } else if (mensagens[i].type === "message") {
 
-            tela.innerHTML += `<li data-test="message" class="mensagem padrao"> <span>(${mensagens[i].time})</span><b>${mensagens[i].from}</b> para <b>${mensagens[i].to}</b>:  ${mensagens[i].text}</li>`;
+            tela.innerHTML +=
+                `<li data-test="message" class="mensagem padrao">
+             <span>(${mensagens[i].time})</span> <b>${mensagens[i].from}
+             </b> para <b>${mensagens[i].to}</b>
+             :  ${mensagens[i].text}</li>`;
 
-        } else if (mensagens[i].type === "private_message" && (mensagens[i].to === nomeDeUsuario || mensagens[i].from === nomeDeUsuario)) {
+        } else if (mensagens[i].type === "private_message" &&
+            (mensagens[i].to === nomeDeUsuario ||
+                mensagens[i].from === nomeDeUsuario)) {
 
-            tela.innerHTML += `<li data-test="message" class="mensagem reservado"> <span>(${mensagens[i].time})</span><b>${mensagens[i].from}</b> reservadamente para <b>${mensagens[i].to}</b>:  ${mensagens[i].text}</li>`;
+            tela.innerHTML +=
+                `<li data-test="message" class="mensagem reservado">
+            <span>(${mensagens[i].time})</span><b>${mensagens[i].from}
+            </b> reservadamente para <b>${mensagens[i].to}</b>
+            :  ${mensagens[i].text}</li>`;
 
         }
     }
@@ -52,7 +68,7 @@ function recarregar() {
 
 
 function statusAtivo() {
-    const promessa = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', nome)
+    const promessa = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', nome);
     promessa.catch(desconectado);
 }
 
@@ -70,20 +86,17 @@ function enviar() {
         to: "Todos",
         text: msg,
         type: "message"
-    }
-    const resposta = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', objMsg);
+    };
+    const respostinha = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', objMsg);
 
-    resposta.then(carregarMensagens);
-    resposta.catch(desconectado);
+    respostinha.then(carregarMensagens);
+    respostinha.catch(desconectado);
     document.querySelector('input').value = '';
 }
 
 
 function descerParaNovaMsg() {
-    if (lastMsg === undefined) {
-        lastMsg = tela.lastElementChild.innerHTML;
-        tela.lastElementChild.scrollIntoView();
-    } else if (lastMsg !== tela.lastElementChild.innerHTML) {
+    if (lastMsg === undefined || lastMsg !== tela.lastElementChild.innerHTML) {
         lastMsg = tela.lastElementChild.innerHTML;
         tela.lastElementChild.scrollIntoView();
     }
